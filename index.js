979dbecf7b7499fa162c400c90b52d1020fb2abd
safwansadi <safwaniftekher@gmail.com>
2021-03-14 00:31:29 +0600

    published a package

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var _ = require('underscore');
-
-var result = _.contains([1,2,3], 3);
+//var _ = require('underscore');
+var addNumber = require("@safwan-sadi/add-numb");
+//const { add } = require('./add-numb-1.0.0');
+//var result = _.contains([1,2,3], 3);
+//console.log(result);
+var result = addNumber.add(2, 5);
 console.log(result);
